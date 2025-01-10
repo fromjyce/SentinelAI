f4c7be029fee729eaab50a79d80743d0c8ee6c5d
Jayashre <jaya2004kra@gmail.com>
2025-01-10 08:31:12 +0530

    added footer

---
+++ frontend/components/UpdateFooter.js
@@ -0,0 +1,15 @@
+'use client';
+
+const UpdateFooter = () => {
+  return (
+    <footer className="w-full bg-[#890408] text-[#ffffff] py-1 urbanist">
+      <div className="text-center">
+        <p className="text-base font-medium">
+          Developed by <strong className="space_grotesk">TEAM SIMPSONS</strong> for <strong className="space_grotesk">CODE RED&apos;25 Hackathon</strong>.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default UpdateFooter;
