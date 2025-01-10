c51eeab82a6309ef94565944a1fd20497fbfc8b1
Jayashre <jaya2004kra@gmail.com>
2025-01-10 09:14:14 +0530

    added header

--- frontend/pages/upload-dashboard.js
+++ frontend/pages/upload-dashboard.js
@@ -1,5 +1,10 @@
+import Head from "next/head"
+
 export default function Dashboard () {
     return (
+        <>
+        <Head><title>SentinelAI | Your Network Dashboard</title></Head>
         <h1>Hi</h1>
+        </>
     )
 }
